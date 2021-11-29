652275d9e9c308b9a2235eeafbf91c1b0bab3a1f
huchenmin <huchenmin@126.com>
2021-11-30 00:55:11 +0800

    fix

--- jni/CubeTaskCallBackImpl.h
+++ jni/CubeTaskCallBackImpl.h
@@ -1,5 +1,10 @@
 #ifndef _CUBETASKCALLBACK_H_
-#define _CUBETASKCALLBACK_H_#include "HCDNDownloader.h"#include <base/include/AutoLock.h>#include "HCDNDownloaderTaskCallBackImpl.h"#include <jni.h>#include <map>
+#define _CUBETASKCALLBACK_H_
+#include "HCDNDownloader.h"
+#include <base/include/AutoLock.h>
+#include "HCDNDownloaderTaskCallBackImpl.h"
+#include <jni.h>
+#include <map>
 
 class CCubeTaskCallBackImpl : public IHCDNDownloaderTaskCallBack
 {
@@ -17,9 +22,12 @@ public:
 	virtual void OnProcess(IHCDNDownloaderTask* pTask, unsigned long long& ullTotal, unsigned long long& ullPos);		// 任务下载进度， total，总长度， pos，当前下载长度， 单位字节 
 	virtual void OnStartTaskSuccess(IHCDNDownloaderTask* pTask);														//hcdn 任务启动成功回调 
 
-private:	psl::CLock m_lock;
+private:
+	psl::CLock m_lock;
 	std::map<IHCDNDownloaderTask*,CHCDNDownloaderTaskCallBackImplPtr> m_taskcbmap;
 };
-extern CCubeTaskCallBackImpl* gCubeTaskCallBackImpl;extern IHCDNDownloaderCreator* gIHCDNDownloaderCreator;
+
+extern CCubeTaskCallBackImpl* gCubeTaskCallBackImpl;
+extern IHCDNDownloaderCreator* gIHCDNDownloaderCreator;
 
 #endif
